2b29df0c5d2cf3be95e11b3f53f31c1ae2f16533
Fabio Lanza <fabio.lanza4@gmail.com>
2023-04-13 18:16:30 -0700

    first commit

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+
+function Header() {
+
+  return (
+    <div className='header-container'>
+    <h2>Fake Store</h2>
+    <AiOutlineShoppingCart className='cart-item'/>
+
+    </div>
+  )
+}
+
+export default Header
